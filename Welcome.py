5cc0c86c647de95d625aed6e8a52d22daa4c809b
Thomas Cibils <thomas.cibils@gmail.com>
2023-02-27 17:19:10 +0100

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,19 +1,19 @@
 import streamlit as st
-# import gspread as gs
+import gspread as gs
 import pandas as pd
 
 # --------- Import gsheet with recipes
 # Use json identifier
-# gc = gs.service_account(filename='recipe-finder-379006-704429557353.json')
+gc = gs.service_account(filename='recipe-finder-379006-704429557353.json')
 # Open the spreadsheet link to the input form
-# sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1oR39MoNU1HP04L8k4zmbt4BdQANouu7KlkEfubFBFMc/edit?usp=sharing')
+sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1oR39MoNU1HP04L8k4zmbt4BdQANouu7KlkEfubFBFMc/edit?usp=sharing')
 # Open the correct worksheet
-# ws = sh.worksheet('Réponses au formulaire 1')
+ws = sh.worksheet('Réponses au formulaire 1')
 # Convert it in pandas dataframe to be able to work with it
-# df = pd.DataFrame(ws.get_all_records())
+df = pd.DataFrame(ws.get_all_records())
 
 # --------- Import csv with recipes
-df = pd.read_csv('recipe.csv', encoding='cp1252')
+# df = pd.read_csv('recipe.csv', encoding='cp1252')
 
 # --------- Sidebar and global things
 
