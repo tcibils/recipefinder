c07ae00b2761c84723701ac37574a4cd3c197aa3
Thomas Cibils <thomas.cibils@gmail.com>
2023-03-01 09:03:45 +0100

    Ajout ingrédients

--- pages/2_Methode Aurore.py
+++ pages/2_Methode Aurore.py
@@ -57,13 +57,13 @@ with col3:
     proteines_not_available = list(set(proteines_options) - set(proteines_choisis))
 
 with col1:
-    laitages_options = ('Ricotta', 'Gruyere', 'Creme', 'Lait', 'Yaourt nature')
+    laitages_options = ('Ricotta', 'Gruyere', 'Creme', 'Lait', 'Yaourt nature', 'Mozarella')
     laitages_preselected = ('Gruyere', 'Lait')
     laitages_choisis = st.multiselect(label='Laitages au frigo', options=laitages_options, default=laitages_preselected)    
     laitages_not_available = list(set(laitages_options) - set(laitages_choisis))
 
 with col2:
-    feculents_options = ('Pates', 'Riz', 'Quinoa', 'Pommes de terre', 'Pain sec ou panure', 'Puree', 'Semoule', 'Lentilles beluga', 'Lentilles corail', 'Pois chiches')
+    feculents_options = ('Pates', 'Riz', 'Quinoa', 'Pommes de terre', 'Pain sec ou panure', 'Puree', 'Semoule', 'Lentilles brunes', 'Lentilles beluga', 'Lentilles corail', 'Pois chiches')
     feculents_preselected = ('Pates', 'Riz', 'Quinoa', 'Pommes de terre', 'Pain sec ou panure', 'Puree', 'Semoule', 'Lentilles beluga', 'Lentilles corail')
     feculents_choisis = st.multiselect(label='Légumineuses et féculents', options=feculents_options, default=feculents_preselected)    
     feculents_not_available = list(set(feculents_options) - set(feculents_choisis))
