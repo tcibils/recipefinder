87c4ca65beb7b3d3a18cbb3ecc6df644e60d8cce
Thomas Cibils <thomas.cibils@gmail.com>
2023-03-12 22:17:24 +0100

    Curry

--- pages/5_External links.py
+++ pages/5_External links.py
@@ -4,3 +4,4 @@ st.title("External pages")
 
 st.write("Recettes d'isa: https://cuisinepourbidnonor.blogspot.com/?m=1")
 st.write("Cheat code: tous les aliments de la même couleur vont bien ensemble. Mauro Colagreco")
+st.write("Utiliser la pâte de curry Blue Elephant pour les recettes") 
