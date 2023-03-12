5ef8ab0c83346943827a7f2b703e4597b69c2f0d
Thomas Cibils <thomas.cibils@gmail.com>
2023-03-12 20:37:40 +0100

    Adding Mauro Colagreco quote

--- pages/5_External links.py
+++ pages/5_External links.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
 st.title("External pages")
+
 st.write("Recettes d'isa: https://cuisinepourbidnonor.blogspot.com/?m=1")
+st.write("Cheat code: tous les aliments de la même couleur vont bien ensemble. Mauro Colagreco")
