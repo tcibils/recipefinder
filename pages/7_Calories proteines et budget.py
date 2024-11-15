a24a17245874299a1fbc9a71e48b2e6d94ed7e3a
Thomas Cibils <thomas.cibils@gmail.com>
2024-11-15 21:28:50 +0100

    Update 7_Calories proteines et budget.py

--- pages/7_Calories proteines et budget.py
+++ pages/7_Calories proteines et budget.py
@@ -5,13 +5,9 @@ import plotly.express as px
 st.set_page_config(page_title='Cibils Recipe', page_icon='logo/chosen_logo_squared.png', layout='wide')
 
 st.title("Nutrition")
-st.subheader("Règles générales")
-st.write("C'est pas très compliqué tout ça.")
-st.write("La graisse, c'est de l'énergie stockée. L'unité c'est les calories, 1kg de gras c'est 7700 calories. L'énergie que tu manges, c'est compté en calories positives. De base tu brules environ 2000 calories par jour. Bouger et faire du cardio, ça fait des calories négatives. Si tu manges moins que ta dépense, tu perds du gras. Sinon tu en prends. Donc soit bouge, soit mange moins de calories, et tu maigriras.")
-st.write("Le muscle, ça se déchire avec l'exercice. Si tu manges des protéines, ça le répare et le fait grossir.")
-st.write("D'où les visualisations ci-dessous : tu peux voir les aliments avec beaucoup de protéines pour peu de calories. J'ai aussi ajouté la dimension prix...")
 
-tab1, tab2, tab3 = st.tabs(["3D Data", "2D Data", "Input form"])
+
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["2D Data", "3D Data", "Full database", "Concept", "Input form"])
 
 
 
@@ -20,7 +16,7 @@ tab1, tab2, tab3 = st.tabs(["3D Data", "2D Data", "Input form"])
 conn = st.connection("gsheetsAlimentsProteines", type=GSheetsConnection)
 df = conn.read()
 
-with tab1:
+with tab2:
     st.subheader("Matrice")
     
     # see https://plotly.com/python/3d-charts/
@@ -35,9 +31,7 @@ with tab1:
 
     st.plotly_chart(figThreeD, use_container_width=True)
 
-    st.write(df)
-
-with tab2:
+with tab1:
     st.subheader("Proteine to Price")
     figTwoDOne = px.scatter(
     	df,
@@ -62,8 +56,16 @@ with tab2:
         
     st.plotly_chart(figTwoDTwo, use_container_width=True)
 
+with tab3:
     st.write(df)
 
+with tab4:
+    st.subheader("Règles générales")
+    st.write("C'est pas très compliqué tout ça.")
+    st.write("La graisse, c'est de l'énergie stockée. L'unité c'est les calories, 1kg de gras c'est 7700 calories. L'énergie que tu manges, c'est compté en calories positives. De base tu brules environ 2000 calories par jour. Bouger et faire du cardio, ça fait des calories négatives. Si tu manges moins que ta dépense, tu perds du gras. Sinon tu en prends. Donc soit bouge, soit mange moins de calories, et tu maigriras.")
+    st.write("Le muscle, ça se déchire avec l'exercice. Si tu manges des protéines, ça le répare et le fait grossir.")
+   st.write("D'où les visualisations sur cette page : tu peux voir les aliments avec beaucoup de protéines pour peu de calories. J'ai aussi ajouté la dimension prix...")
 
-with tab3:
+
+with tab5:
     st.components.v1.iframe(src="https://docs.google.com/forms/d/e/1FAIpQLScZHgBQCmItwf1iI0_FuqO4VRCmDLlfs4YEts8KbGWvlmswIQ/viewform?embedded=true", width=700, height=3800, scrolling=True)
